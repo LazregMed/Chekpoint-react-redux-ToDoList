ad87c1692d156e69713518de3b862e70de0b76e2
LazregMed <med034406@gmail.com>
2022-08-10 16:52:12 +0100

    ToDoList

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {useSelector} from 'react-redux'
+import TodoList from './Components/TodoList';
+import AddTodos from './Components/AddTodo';
+import Filtered from './Components/Filter';
+
+function App() {
+  let todos = useSelector(state=> state.Todo.todos)
+  let Filter = useSelector(state=> state.Todo.Filter)
+  return (
+    <div className="App">
+     <h1>TodoList</h1>
+     <hr/>
+     <h2> Checkpoint react-redux</h2>
+     <hr/>
+     <AddTodos/>
+     <TodoList todos={(Filter=='Done')? todos.filter(el=> el.isDone== true): (Filter=='UnDone')? todos.filter(el=> el.isDone==false): todos} />
+      <hr/>
+      <Filtered/>
+
+    </div>
+  );
+}
+
+export default App;
+
